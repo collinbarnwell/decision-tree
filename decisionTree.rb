7a54b2266d3919ad1341147adbe2a3cbab8433e7
Collin Barnwell <cbarnwell@g2crowd.com>
2015-05-01 15:43:02 -0500

    turn-in ready

--- decisionTree.rb
+++ decisionTree.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 SPLIT_MAX = 1
 MAX_ITERATIONS = 10000
-RESTRICT_PRINT_TO_16 = true
+RESTRICT_PRINT_TO_16 = false
 
 def median(ary)
   # from http://stackoverflow.com/questions/21487250/find-the-median-of-an-array
@@ -10,6 +10,14 @@ def median(ary)
   sorted = ary.sort
   ary.length.odd? ? sorted[mid] : 0.5 * (sorted[mid] + sorted[mid - 1])
 end
+
+class Array; def in_groups(num_groups)
+  # from http://stackoverflow.com/questions/12374645
+  # /splitting-an-array-into-equal-parts-in-ruby
+  return [] if num_groups == 0
+  slice_size = (self.size/Float(num_groups)).ceil
+  self.each_slice(slice_size).to_a
+end; end
 
 class TreeBuilder
   def self.recursiveBuild(datas, atts_left, numSplits)
@@ -98,7 +106,70 @@ class TreeBuilder
     return n
   end
 
-  def self.buildAndTest(csvin, csvtest)
+  def self.learningCurve(csvin, validate)
+    num_examples = 50000
+    num_trains = 0
+
+    while num_trains < num_examples - 5000
+      num_trains += 5000
+      num_tests = num_examples - num_trains
+
+      validation_data = []
+
+      count = 0
+      datas = []
+      iteration = 0
+      CSV.foreach(validate) do |row|
+        iteration+=1
+
+        next if iteration == 1 or iteration == 2
+
+        if iteration >= num_tests
+          validation_data << Datum.new(row)
+        else
+          datas << Datum.new(row)
+        end
+      end
+
+      tree = buildTree(csvin, num_trains)
+      tree.prune(@@datas, validation_data)
+
+      correct = 0
+      wrong = 0
+      datas.each do |d|
+        actual = tree.test(d)
+        if d.output == actual
+          correct+=1
+        else
+          wrong+=1
+        end
+      end
+
+      puts "Num Trains: #{num_trains} - #{100*correct.to_f/(correct+wrong).to_f}%"
+    end
+  end
+
+  def self.testOn(tree, datas, label, print = false)
+    correct = 0
+    wrong = 0
+    datas.each do |d|
+      actual = tree.test(d)
+      if d.output == actual
+        correct+=1
+      else
+        wrong+=1
+      end
+    end
+
+    tree.dnfprint("", 1) if print
+
+    puts label
+    puts "Correct: #{correct}, Wrong: #{wrong} => \
+    #{100*correct.to_f/(correct+wrong).to_f}%"
+    puts "Splits: #{tree.count}"
+  end
+
+  def self.buildAndTest(csvin, csvtest, print = false)
     tree = buildTree(csvin)
 
     # get data
@@ -111,42 +182,47 @@ class TreeBuilder
       break if iteration >= MAX_ITERATIONS
     end
 
-    # test original tree
-    correct = 0
-    wrong = 0
-    datas.each do |d|
-      actual = tree.test(d)
-      if d.output == actual
-        correct+=1
-      else
-        wrong+=1
-      end
-    end
-    puts "ORIGINAL TREE: "
-    puts "Correct: #{correct}, Wrong: #{wrong} => \
-    #{100*correct.to_f/(correct+wrong).to_f}%"
+    dsplit = datas.in_groups(2)
 
-    # pruned tree
-    tree.prune(@@datas)
-    
-    correct = 0
-    wrong = 0
-    datas.each do |d|
-      actual = tree.test(d)
-      if d.output == actual
-        correct+=1
-      else
-        wrong+=1
-      end
-    end
-    puts "PRUNED TREE: "
-    puts "Correct: #{correct}, Wrong: #{wrong} => \
-    #{100*correct.to_f/(correct+wrong).to_f}%"
+    testOn(tree, dsplit[1], "ORIGINAL TREE:", print)
 
+    # pruned tree once
+    tree.prune(@@datas, dsplit.first) 
+    testOn(tree, dsplit[1], "PRUNED TREE:", print)
     nil
   end
 
-  def self.buildTree(csvin)
+  def self.predict(train, validate, test, out)
+    tree = buildTree(train)
+
+    datas = []
+    iteration = 0
+    CSV.foreach(validate) do |row|
+      iteration+=1
+      next if iteration == 1 or iteration == 2
+      datas << Datum.new(row)
+      break if iteration >= MAX_ITERATIONS
+    end
+
+    tree.prune(@@datas, datas)
+
+    CSV.open(out, "w") do |csv|
+      iter = 0
+      CSV.foreach(test) do |row|
+        if iter < 2
+          iter += 1
+          next
+        end
+
+        d = Datum.new(row)
+        o = tree.test(d)
+        row[row.length - 1] = o
+        csv << row
+      end
+    end
+  end
+
+  def self.buildTree(csvin, max_it = MAX_ITERATIONS)
     datas = []
     @@keys = []
     @@nom_choices = Hash.new(nil)
@@ -192,7 +268,7 @@ class TreeBuilder
       end
 
       datas << Datum.new(row)
-      break if iteration >= MAX_ITERATIONS
+      break if iteration >= max_it
     end
 
     @@datas = datas
@@ -294,7 +370,7 @@ class Node < TreeBuilder
     end
   end
 
-  def prune(datas)
+  def prune(datas, vdata)
     if @children.first.isLeaf # means all children are leaves
       # replace self with leaf that correctly classifies majority
       output_counts = Hash.new(0)
@@ -315,29 +391,71 @@ class Node < TreeBuilder
     if @nominal
       @children.each_with_index do |child, ind|
         new_data = datas.select {|d| d.vals[@att] == @@nom_choices[@att][ind] }
-        cprune = child.prune(new_data)
-        @children[ind] = cprune if cprune
+        new_vdata = vdata.select {|d| d.vals[@att] == @@nom_choices[@att][ind] }
+
+        if cprune = child.prune(new_data, new_vdata)
+          self.checkForReplace(cprune, new_vdata, ind) 
+        end
       end
+
     else
       # continuous
       new_data = datas.select do |d|
         d.vals[@att].is_a?(String) or d.vals[@att] < @thresh
       end
-      cprune = @children.first.prune(new_data)
-      @children[0] = cprune if cprune
+      new_vdata = vdata.select do |d|
+        d.vals[@att].is_a?(String) or d.vals[@att] < @thresh
+      end
 
+      cprune = @children.first.prune(new_data, new_vdata)
+      @children[0] = cprune if cprune
+      if cprune = @children[0].prune(new_data, new_vdata)
+        self.checkForReplace(cprune, new_vdata, 0)
+      end
+
+      # Second cild
       new_data = datas.select do 
         |d| !(d.vals[@att].is_a?(String)) and d.vals[@att] >= @thresh
       end
-      cprune = @children[1].prune(new_data)
-      @children[1] = cprune if cprune
+      new_vdata = vdata.select do |d|
+        d.vals[@att].is_a?(String) or d.vals[@att] >= @thresh
+      end
+      
+      if cprune = @children[1].prune(new_data, new_vdata)
+        self.checkForReplace(cprune, new_vdata, 1) 
+      end
     end
 
     return nil
   end
 
+  def checkForReplace(cprune, vdata, idx)
+    originalPercentage = self.getTestPercentage(vdata)
+    oldNode = @children[idx]
+    @children[idx] = cprune
+    prunePercentage = self.getTestPercentage(vdata)
+
+    if originalPercentage > prunePercentage
+      @children[idx] = oldNode
+    end
+  end
+
+  def getTestPercentage(vdata)
+    correct = 0
+    vdata.each do |v|
+      correct+=1 if self.test(v) == v.output
+    end
+    correct.to_f/vdata.count.to_f
+  end
+
   def isLeaf()
     false
+  end
+
+  def count
+    count = 0
+    @children.each {|c| count += c.count} if @children
+    return count + 1
   end
 end
 
@@ -363,11 +481,15 @@ class Leaf < Node
     end
   end
 
-  def prune(datas)
+  def prune(datas, vdata)
     return self
   end
 
   def isLeaf()
     true
   end
+end
+
+def t
+  TreeBuilder.buildAndTest("btrain.csv", "bvalidate.csv")
 end
